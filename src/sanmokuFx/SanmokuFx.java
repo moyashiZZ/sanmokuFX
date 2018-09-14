62452d427cacfe333af97f342668eef7487476dc
moyashiZZ <zababamoyashi0809@gmail.com>
2018-09-14 22:27:37 +0900

    ウィンドウアイコン、ウィンドウタイトルを追加

--- src/sanmokuFx/SanmokuFx.java
+++ src/sanmokuFx/SanmokuFx.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class SanmokuFx extends Application {
@@ -13,7 +14,9 @@ public class SanmokuFx extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        final Image windowIcon = new Image("image/windowIcon.jpg");
+        stage.getIcons().add(windowIcon);
+        stage.setTitle("三目並べ");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
